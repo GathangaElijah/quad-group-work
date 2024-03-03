84cc3ee2cabbff7fc5f7d94ca7ffb039fbbed427
egathang <elyg3672@gmail.com>
2024-03-03 21:32:00 +0300

    quadC program

---
+++ quadC.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "fmt"
+
+func QuadC(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
+			if i == 1 && j == 1 {
+				//First corner
+				fmt.Print("A")
+			} else if i == 1 && j == x {
+				//Second corner
+				fmt.Print("A")
+			} else if i == y && j == 1 {
+				//Third corner
+				fmt.Print("C")
+			} else if i == y && j == x {
+				fmt.Print("C")
+			} else if i == 1 || i == y || j == 1 || j == x {
+				fmt.Print("B")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		println()
+	}
+}
